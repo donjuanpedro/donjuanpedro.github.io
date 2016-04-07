47dd4d0d270d4e75e3096aa73db0ee3c3b5b094d
Walker <jwalkerlyle@gmail.com>
2016-04-07 13:29:07 -0500

    finalproject update
    rewrote index.html entirely

---
+++ AustinCodingAcademy_Spring2016/Week8/FinalProject/js/animate-scroll.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $(window).scroll(function() {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if(verticalScroll >= 200) {
+
+            $('#pancakes').addClass('animated flipInX infinite');
+        }
+
+    });
+    
+    $(window).scroll(function() {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if(verticalScroll >= 200) {
+
+            $('#my_jello').addClass('animated jello infinite');
+        }
+
+    });
+    
+}); 
+
