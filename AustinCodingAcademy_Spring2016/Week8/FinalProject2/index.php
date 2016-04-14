8106b244c9b69cb1a89ac426cca29528a77bb577
Walker <jwalkerlyle@gmail.com>
2016-04-14 14:37:47 -0500

    finalized woodshop

--- AustinCodingAcademy_Spring2016/Week8/FinalProject2/index.php
+++ AustinCodingAcademy_Spring2016/Week8/FinalProject2/index.php
@@ -1,3 +1,45 @@
+<?php
+	if (isset($_POST["submit"])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$message = $_POST['message'];
+		$human = intval($_POST['human']);
+		$from = 'Contact Form'; 
+		$to = 'jwalkerlyle@gmail.com'; 
+		$subject = 'Message from walkerlyle.com ';
+		
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+ 
+		// Check if name has been entered
+		if (!$_POST['name']) {
+			$errName = 'Please enter your name';
+		}
+		
+		// Check if email has been entered and is valid
+		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+			$errEmail = 'Please enter a valid email address';
+		}
+		
+		//Check if message has been entered
+		if (!$_POST['message']) {
+			$errMessage = 'Please enter your message';
+		}
+		//Check if simple anti-bot test is correct
+		if ($human !== 5) {
+			$errHuman = 'Your anti-spam is incorrect';
+		}
+ 
+// If there are no errors, send the email
+if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+	if (mail ($to, $subject, $body, $from)) {
+		$result='<div class="alert alert-success">Thank You, I will be in touch shortly!</div>';
+	} else {
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	}
+}
+	}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -569,28 +611,47 @@
 					<p class="st-website"><i class="fa fa-globe"></i> <strong>southhousetunes.com</strong></p>
 				</div>
                 <div class="col-sm-7 col-sm-offset-1">
-					<form action="php/send-contact.php" class="contact-form" name="contact-form" method="post">
-						<div class="row">
-							<div class="col-sm-6">
-								<input type="text" name="name" required="required" placeholder="Name*">
-							</div>
-							<div class="col-sm-6">
-								<input type="email" name="email" required="required" placeholder="Email*">
-							</div>
-							<div class="col-sm-6">
-								<input type="text" name="subject" placeholder="Subject">
-							</div>
-							<div class="col-sm-6">
-								<input type="text" name="website" placeholder="Website">
-							</div>
-							<div class="col-sm-12">
-								<textarea name="message" required="required" cols="30" rows="7" placeholder="Message*"></textarea>
-							</div>
-							<div class="col-sm-12">
-								<input type="submit" name="submit" value="Send Message" class="btn btn-send">
-							</div>
-						</div>
-					</form>
+					
+<form class="form-horizontal" role="form" method="post" action="index.php">
+	<div class="form-group">
+		<label for="name" class="col-sm-2 control-label">Name</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+			<?php echo "<p class='text-danger'>$errName</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="email" class="col-sm-2 control-label">Email</label>
+		<div class="col-sm-10">
+			<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+			<?php echo "<p class='text-danger'>$errEmail</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="message" class="col-sm-2 control-label">Message</label>
+		<div class="col-sm-10">
+			<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+			<?php echo "<p class='text-danger'>$errMessage</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+		<div class="col-sm-10">
+			<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+			<?php echo "<p class='text-danger'>$errHuman</p>";?>
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+		</div>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 col-sm-offset-2">
+			<?php echo $result; ?>	
+		</div>
+	</div>
+</form> 
 				</div>
             </div>
         </div>
