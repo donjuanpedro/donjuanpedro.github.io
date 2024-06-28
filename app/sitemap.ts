0cfe6dd7cd6bf78ec470ebbfc12db239cb93c0c0
Walker Lyle <walker@Walkers-Air.attlocal.net>
2024-06-28 15:10:13 -0500

    add out

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,9 @@
-import { getBlogPosts } from 'app/db/blog';
-
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `https://walkerlyle.com/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
 
-  let routes = ['', '/blog', '/guestbook', '/uses', '/work'].map((route) => ({
+  let routes = ['', '/uses', '/work'].map((route) => ({
     url: `https://walkerlyle.com${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes];
 }
