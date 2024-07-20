b28363f5372122fc584a1402f5b7010ee76c0083
Walker Lyle <walker@Walkers-Air.attlocal.net>
2024-07-20 14:51:25 -0500

    work edits 2

--- .next/server/chunks/ssr/_8efe49._.js
+++ .next/server/chunks/ssr/_8efe49._.js
@@ -323,7 +323,7 @@ function Page() {
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$future$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                             className: "prose prose-neutral dark:prose-invert mb-4",
-                            children: "I'm an Austin-based web developer, avid fly-fisherman, and amateur barbecue competitor. Other obsessions include American history between 1781-1818, songwriting, and literature."
+                            children: "I'm an Austin-based web developer, avid fly fisherman, and amateur barbecue competitor. Other obsessions include American history between 1781-1818, songwriting, and literature."
                         }, void 0, false, {
                             fileName: "[project]/app/page.tsx",
                             lineNumber: 26,
--- .next/server/chunks/ssr/_b796fd._.js
+++ .next/server/chunks/ssr/_b796fd._.js
@@ -306,7 +306,7 @@ function WorkPage() {
                     }, this),
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$future$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                         className: "text-neutral-600 dark:text-neutral-400 text-sm",
-                        children: "Front-End Web Developer | 2023 - Present"
+                        children: "Frontend Developer | 2023 - Present"
                     }, void 0, false, {
                         fileName: "[project]/app/work/page.tsx",
                         lineNumber: 27,
@@ -396,7 +396,7 @@ function WorkPage() {
                     }, this),
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$future$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                         className: "text-neutral-600 dark:text-neutral-400 text-sm",
-                        children: "Front-End Web Developer | 2016-2021"
+                        children: "Frontend Developer | 2016-2021"
                     }, void 0, false, {
                         fileName: "[project]/app/work/page.tsx",
                         lineNumber: 51,
