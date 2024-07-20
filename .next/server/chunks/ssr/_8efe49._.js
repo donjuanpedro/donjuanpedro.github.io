bb989650ba153d85678d7f03e6ab53539d93cf10
Walker <jwalkerlyle@gmail.com>
2024-07-20 14:47:44 -0500

    Revert "work edits"

--- .next/server/chunks/ssr/_8efe49._.js
+++ .next/server/chunks/ssr/_8efe49._.js
@@ -20,9 +20,6 @@ const navItems = {
     },
     'https://blog.walkerlyle.com/': {
         name: 'blog'
-    },
-    '/contact': {
-        name: 'contact'
     }
 };
 function Navbar() {
@@ -42,28 +39,28 @@ function Navbar() {
                             children: name
                         }, path, false, {
                             fileName: "[project]/app/components/nav.tsx",
-                            lineNumber: 29,
+                            lineNumber: 26,
                             columnNumber: 17
                         }, this);
                     })
                 }, void 0, false, {
                     fileName: "[project]/app/components/nav.tsx",
-                    lineNumber: 26,
+                    lineNumber: 23,
                     columnNumber: 11
                 }, this)
             }, void 0, false, {
                 fileName: "[project]/app/components/nav.tsx",
-                lineNumber: 22,
+                lineNumber: 19,
                 columnNumber: 9
             }, this)
         }, void 0, false, {
             fileName: "[project]/app/components/nav.tsx",
-            lineNumber: 21,
+            lineNumber: 18,
             columnNumber: 7
         }, this)
     }, void 0, false, {
         fileName: "[project]/app/components/nav.tsx",
-        lineNumber: 20,
+        lineNumber: 17,
         columnNumber: 5
     }, this);
 }
@@ -326,7 +323,7 @@ function Page() {
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$future$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                             className: "prose prose-neutral dark:prose-invert mb-4",
-                            children: "I'm an Austin-based web developer, avid fly fisherman, and amateur barbecue competitor. Other obsessions include American history between 1781-1818, songwriting, and literature."
+                            children: "I'm an Austin-based web developer, avid fly-fisherman, and amateur barbecue competitor. Other obsessions include American history between 1781-1818, songwriting, and literature."
                         }, void 0, false, {
                             fileName: "[project]/app/page.tsx",
                             lineNumber: 26,
